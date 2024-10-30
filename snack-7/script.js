a006e569ba65505ac214b49279d2f71bd1198407
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-10-30 15:36:05 +0100

    ADD snack 7 solution

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,23 +1,25 @@
 const students = [
-  {
-    id: 1,
-    name: 'Marco Lanci',
-    age: 32,
-    class: '3C'
-  },
-  {
-    id: 2,
-    name: 'Mario Banfi',
-    age: 34,
-    class: '4A'
-  },
-  {
-    id: 3,
-    name: 'Luigi Banzi',
-    age: 33,
-    class: '5B'
-  },
-];
+    {
+        id: 1,
+        name: 'Marco Lanci',
+        age: 32,
+        class: '3C',
+    },
+    {
+        id: 2,
+        name: 'Mario Banfi',
+        age: 34,
+        class: '4A',
+    },
+    {
+        id: 3,
+        name: 'Luigi Banzi',
+        age: 33,
+        class: '5B',
+    },
+]
 
 // Recupera i dati dello studente con id 2
+const found = students.find((el) => el.id === 2)
+console.log(found)
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
